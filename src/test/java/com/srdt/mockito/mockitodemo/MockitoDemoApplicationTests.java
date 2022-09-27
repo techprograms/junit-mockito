d1a8ac87760eaa6fe011b8c1c171f3985511f511
techprograms <tech.gpr@gmail.com>
2022-09-27 10:18:38 +0530

    initial commit for mockito

---
+++ src/test/java/com/srdt/mockito/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srdt.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
